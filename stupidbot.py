cc7c91b90ac6e7f6f4c76f85fba15a17b5d82f84
ChiefZer0 <35029705+ChiefZer0@users.noreply.github.com>
2018-04-27 22:22:48 +0200

    Add files via upload

---
+++ stupidbot.py
@@ -0,0 +1,18 @@
+#Stupid Program by Chief.
+greeting = "Good morning"
+greeting_1 = "Good afternoon"
+greeting_2 = "Good evening"
+greeting_3 = "Good night"
+name = input("Please enter your name_")
+print(greeting_3 + ' ' + name)
+question1 = input("What day is it today?")
+answer = "Oh sorry, the day is..."
+print(answer + ' ' + question1 )
+question2 = input("Wow, I just realised I am just a stupid python thing created by Chief.")
+print(question2)
+question3 = input("Well anyways, give me a name!")
+namebot = "My name is"
+namebot2 = "Yes! My name is"
+print(namebot + ' ' +question3 + ' ' + namebot2 + ' ' + question3)
+print("Thanks for using Stupid_Bot by Chief! Shutting down...")
+#End of Stupid Program by Chief.
